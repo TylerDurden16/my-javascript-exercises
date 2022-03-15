f35696098e9958d53fcfdebd4c397f638d0a24bb
TylerDurden16 <larrysellers16@gmail.com>
2022-03-15 16:04:46 -0700

    updated sumAll.js, passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,26 +3,30 @@ Takes in 2 integers
 Use Math.max and Math.min on arguments and store in variables
 create empty sum variable
 WHILE (integers aren't  added up) DO
-add numbers between the 2 parameters and store in sum variable
+Add numbers between the 2 parameters to an array
 END
+Use reduce method on array
 END*/
 const sumAll = function(x,y) {
+    if (x < 0 || y < 0 || typeof(x) !== "number"|| typeof(y) !== "number") {
+        return "ERROR";
+    }
+     
     let minNum = Math.min(x, y);
     const maxNum = Math.max(x, y);
-    let sum = 0;
     let range = [];
- 
-    for(let i = minNum; i <= maxNum; i++) {
-           range.push(i);
 
+    for(; minNum <= maxNum; minNum++) {
+        range.push(minNum);
     }
-    //console.log(range.reduce(addNums));
-   return range.reduce((total, num) => {
-       return total + num;
-        
-    });
+        return range.reduce((total, num) => {
+            return total + num;   
+         });
+    }
     
-};
+ 
+    
+
 
 // Do not edit below this line
 module.exports = sumAll;
