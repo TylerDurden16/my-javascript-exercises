92af600af00671688114989ffb9f75135edab6b5
TylerDurden16 <larrysellers16@gmail.com>
2022-04-11 13:33:54 -0700

    updated palindromes.js, removes punctuation and non-letters

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,7 @@
-const palindromes = function (string) {
-    const reverse = string.toLowerCase().replace(/[^A-Za-z0-9]/g, "").split("").reverse().join("");
-    return string === reverse ? true : false;  
-    
+const palindromes = function (str) {
+   const string = str.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
+   const reverse = string.split("").reverse().join("");
+   return string === reverse ? true : false;  
 };
 
 // Do not edit below this line
