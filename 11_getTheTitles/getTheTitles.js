20cd70e52708e0de9089b2d6adfabe5f5e482a5a
TylerDurden16 <larrysellers16@gmail.com>
2022-04-11 18:55:44 -0700

    Added getTheTitles.js, html. Completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    const newArr = [];
+    obj.forEach(book =>newArr.push(book.title));
+    return newArr;
 };
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
