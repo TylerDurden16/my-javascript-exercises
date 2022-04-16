10e48b4fdb270f96390f9ecdf3424affd988c2e4
TylerDurden16 <larrysellers16@gmail.com>
2022-04-16 16:47:15 -0700

    added findTheOldest.js, html. Technically works 100%

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(obj) {
+    let age = [];
+    let objAge = {};
+  
+    const determineAge = obj.map(person => {
+      if (!person.yearOfDeath) {
+        const date = new Date();
+        person.age = date.getFullYear() - person.yearOfBirth;
+      }
+      if (person.age == null) {
+        person.age = person.yearOfDeath - person.yearOfBirth;
+      }
+     return person;
+    })
+    .sort((lastPerson, currentPerson) => {
+      return lastPerson.age > currentPerson.age ? -1 : 1; 
+    });
+    return determineAge.at(0);
+  }
+  const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 // Do not edit below this line
 module.exports = findTheOldest;
